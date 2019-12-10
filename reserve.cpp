36f44c1ab1c89e2ea8394cf1720257db37adb93e
kimjy0126 <kimjy0126@skku.edu>
2019-12-10 11:07:13 +0900

    Added exception for input in reserve.cpp

--- reserve.cpp
+++ reserve.cpp
@@ -36,31 +36,54 @@ void make_reservation(void)
     char dst = ' ';
 
     make_reservation_print_menu(name, date, src, dst);
+    char buf[500];
     printf("Enter your name (Up to 9 characters): ");
-    fgets(name, 10, stdin);
-    if (name[strlen(name) - 1] == '\n') name[strlen(name) - 1] = 0;
+    fgets(buf, 500, stdin);
+    if (strlen(buf) > 10) {
+        printf("Invalid input: The length of name should be less than 9\n");
+        printf("Return to main screen...\n");
+        return;
+    }
+    if (buf[strlen(buf) - 1] == '\n') buf[strlen(buf) - 1] = 0;
+    strncpy(name, buf, 10);
     CLS;
 
     make_reservation_print_menu(name, date, src, dst);
     printf("Enter departure date (1 ~ 31): ");
     scanf("%d", &date);
+    if (date < 1 || date > 31) {
+        printf("Invalid input: The date should be in range from 1 to 31\n");
+        printf("Return to main screen...\n");
+        return;
+    }
     CLS;
 
     make_reservation_print_menu(name, date, src, dst);
     printf("Enter source city (A ~ Z): ");
     scanf("\n%c", &src);
+    if (src < 'A' || src > 'Z') {
+        printf("Invalid input: The source city should be in range from 'A' to 'Z'\n");
+        printf("Return to main screen...\n");
+        return;
+    }
     CLS;
 
     make_reservation_print_menu(name, date, src, dst);
     printf("Enter destination city (A ~ Z): ");
     scanf("\n%c", &dst);
     getchar();
+    if (dst < 'A' || dst > 'Z') {
+        printf("Invalid input: The destination city should be in range from 'A' to 'Z'\n");
+        printf("Return to main screen...\n");
+        return;
+    }
     CLS;
 
     make_reservation_print_menu(name, date, src, dst);
 
     Path* newpath = P->get_path(date, src - 'A', dst - 'A');
    	if(newpath->flight_time == -1){
+        printf("Cannot find path from %c to %c\n", src, dst);
 		delete(newpath);
 	}else{
 		int r_id = T.insert(name, newpath);
